ce235c5aa65aebbbf36c161106ec2f55adbcb1e7
katarzynalam <kasialamczyk@wp.pl>
2023-08-02 13:15:50 +0200

    Creating project

---
+++ SnP-Exercise-1/SnP-Exercise-1.Api/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace SnP_Exercise_1.Api
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
