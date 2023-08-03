704b182e1cadccdd590c2e1156630108d5a610bd
katarzynalam <kasialamczyk@wp.pl>
2023-08-03 14:04:36 +0200

    Transfer code creating UsersCollection to Service and added mapping

--- SnP-Exercise-1/SnP-Exercise-1.Api/Controllers/UsersController.cs
+++ SnP-Exercise-1/SnP-Exercise-1.Api/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SnP_Exercise_1.Services;
+using System;
 
 namespace SnP_Exercise_1.Api.Controllers
 {
@@ -6,21 +8,44 @@ namespace SnP_Exercise_1.Api.Controllers
     [Route("v1/[controller]")]
     public class UsersController : ControllerBase
     {
-        private static readonly UsersCollection Users = new UsersCollection
+        private UserService? _userService;
+        public UsersController(UserService userService)
         {
-            Users = new User[] 
-            { 
-                new User { Name = "Bill Gates", Occupation = "CEO", Active = true },
-                new User { Name = "Issac Newton", Occupation = "Scientist", Active = false },
-                new User { Name = "Jasper Kyd", Occupation = "Music composer", Active = true } 
-            }
-
-        };
-        
-        [HttpGet(Name="GetUsers")]
+            _userService = userService;
+        }
+        [HttpGet(Name = "GetUsers")]
         public UsersCollection Get()
         {
-            return Users;
+            return MapToUsersDTO(_userService.GetAllUsers());
         }
-    }
+        private User MapToUserDTO(SnP_Exercise_1.Services.User user)
+        {
+            return new User
+            {
+                Name = user.FirstName + ' ' + user.LastName,
+                Occupation = user.Occupation.ToString(),
+                Active = user.Active
+            };
+        }
+
+        private UsersCollection MapToUsersDTO(SnP_Exercise_1.Services.UsersCollection users)
+        {
+
+            return new UsersCollection
+            {
+                Users = new User[]{ 
+                    MapToUserDTO(users.Users[0]),
+                    MapToUserDTO(users.Users[1]),
+                    MapToUserDTO(users.Users[2]),
+                }
+            };
+               
+        
+        }
+
+
+
+
+}
+
 }
