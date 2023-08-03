e7412e33acdab76323d22e4e4328355dc2b8ad9a
katarzynalam <kasialamczyk@wp.pl>
2023-08-03 11:59:31 +0200

    Added UserService

---
+++ SnP-Exercise-1/SnP-Exercise-1.Api/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using SnP_Exercise_1.Api;
+
+namespace SnP_Exercise_1.Services
+{
+    public enum Occupation
+    {
+        CEO,
+        Scientist,
+        MusicComposer,
+    }
+
+    public class User
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public Occupation Occupation { get; set; }
+        public bool Active { get; set; }
+        public int? Age { get; set; }
+    }
+    public class UsersCollection
+    {
+        public User?[] Users { get; set; }
+    }
+    public class UserService
+    {
+        private static readonly UsersCollection Users = new UsersCollection
+        {
+            Users = new User[]
+            {
+                new User { FirstName = "Bill", LastName="Gates", Occupation = Occupation.CEO, Active = true },
+                new User { FirstName = "Issac Newton", LastName="Newton", Occupation = Occupation.Scientist, Active = false },
+                new User { FirstName = "Kyd", LastName="Kyd", Occupation = Occupation.MusicComposer, Active = true }
+            }
+
+        };
+        public UsersCollection GetUsers()
+        {
+            return Users;
+        }
+    }
+}
